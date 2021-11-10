3ae4ae46c51b420e8c4c57e5ef1b44f8417d8f6a
waltercdg <walter.de.guzman@eee.upd.edu.ph>
2021-11-10 11:43:33 +0800

    Added database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+	Blog.create!(
+		title: "My blog post #{blog}",
+		body: "There is no hate, only joy | For you are beloved by the goddess | Hero of the dawn, Healer of worlds | Dreams of the morrow hath the shattered soul | Pride is lost | Wings stripped away, the end is nigh"
+	)
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 15
+	)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title #{portfolio_item}",
+		subtitle: "My great service",
+		body: "There is no hate, only joy | For you are beloved by the goddess | Hero of the dawn, Healer of worlds | Dreams of the morrow hath the shattered soul | Pride is lost | Wings stripped away, the end is nigh",
+		main_image: "http://placehold.it/600x400",
+		thumb_image: "http://placehold.it/350x200"
+	)
+end
+
+puts "9 portfolio items created"
