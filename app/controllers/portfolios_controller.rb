df54a3ab3566902c0f889650341b26629103bef3
waltercdg <walter.de.guzman@eee.upd.edu.ph>
2021-11-10 15:55:09 +0800

    Implemented destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,4 +38,13 @@ class PortfoliosController < ApplicationController
   def show
   	@portfolio_items = Portfolio.find(params[:id])
   end
+
+  def destroy
+  	@portfolio_items = Portfolio.find(params[:id])
+
+  	@portfolio_items.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Portfolio was removed." }
+    end
+  end
 end
