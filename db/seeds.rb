ffe5b421807a6768e2fb9682a558c673102f5a42
waltercdg <walter.de.guzman@eee.upd.edu.ph>
2021-11-12 15:00:16 +0800

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+	)
+end
+
+puts "3 topics created"
+
 10.times do |blog|
 	Blog.create!(
 		title: "My blog post #{blog}",
-		body: "There is no hate, only joy | For you are beloved by the goddess | Hero of the dawn, Healer of worlds | Dreams of the morrow hath the shattered soul | Pride is lost | Wings stripped away, the end is nigh"
+		body: "There is no hate, only joy | For you are beloved by the goddess | Hero of the dawn, Healer of worlds | Dreams of the morrow hath the shattered soul | Pride is lost | Wings stripped away, the end is nigh",
+		topic_id: Topic.last.id
 	)
 end
 
@@ -24,10 +33,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
 		title: "Portfolio title #{portfolio_item}",
-		subtitle: "My great service",
+		subtitle: "Ruby on Rails",
+		body: "There is no hate, only joy | For you are beloved by the goddess | Hero of the dawn, Healer of worlds | Dreams of the morrow hath the shattered soul | Pride is lost | Wings stripped away, the end is nigh",
+		main_image: "http://placehold.it/600x400",
+		thumb_image: "http://placehold.it/350x200"
+	)
+end
+
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title #{portfolio_item}",
+		subtitle: "Angular",
 		body: "There is no hate, only joy | For you are beloved by the goddess | Hero of the dawn, Healer of worlds | Dreams of the morrow hath the shattered soul | Pride is lost | Wings stripped away, the end is nigh",
 		main_image: "http://placehold.it/600x400",
 		thumb_image: "http://placehold.it/350x200"
